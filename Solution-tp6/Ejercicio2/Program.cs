59acb032ba7955f6e20b4551d3a9a999ca548c6f
TattySanagua <tati_sanagua@hotmail.com>
2019-05-12 16:21:35 -0300

    Ejercicios 1 y 2

---
+++ Solution-tp6/Ejercicio2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, operacion;
+            int c, n;
+
+            Console.WriteLine("---CALCULADORA BASICA---");
+
+            do
+            {
+                Console.WriteLine("Elija la operacion que quiere realizar: ");
+            
+                Console.WriteLine("Suma Opcion: A");
+                Console.WriteLine("Resta Opcion: B");
+                Console.WriteLine("Multiplicacion Opcion: C");
+                Console.WriteLine("Division Opcion: D");
+            
+                c = char.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese valor de a: ");
+                a = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese valor de b: ");
+                b = double.Parse(Console.ReadLine());
+
+            
+                switch (c)
+                {
+                    case 'A':
+                        operacion = a + b;
+                        Console.Write("Resultado = ");
+                        Console.Write(operacion + "\r\n");
+                        Console.ReadKey(); break;
+                    case 'B':
+                        operacion = a - b;
+                        Console.Write("Resultado = ");
+                        Console.Write(operacion + "\r\n");
+                        Console.ReadKey(); break;
+                    case 'C':
+                        operacion = a * b;
+                        Console.Write("Resultado = ");
+                        Console.Write(operacion + "\r\n");
+                        Console.ReadKey(); break;
+                    case 'D':
+                        operacion = a / b;
+                        Console.Write("Resultado = ");
+                        Console.Write(operacion + "\r\n");
+                        Console.ReadKey(); break;
+                }
+                Console.WriteLine("Desea realiar otra operacion?- Si = 1, No = 0");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n != 0);
+            
+        }
+    }
+}
---
+++ Solution-tp6/Tp6-1-Sanagua/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp6_1_Sanagua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, resto, b=0;
+            string valor = "";
+
+            Console.Write("Ingresar un numero: ");
+            valor = Console.ReadLine();
+            a = Convert.ToInt32(valor);
+
+            while(a != 0)
+            {
+                resto = a % 10;
+                a = a / 10;
+                b = b * 10 + resto;
+
+            }
+            Console.Write("Numero Invertido: ");
+            Console.WriteLine(b);
+            Console.ReadKey();
+
+
+        }
+    }
+}
